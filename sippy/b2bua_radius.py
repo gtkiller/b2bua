ea7f215eafa320b6b08c58757952249c9565ef34
Dmitry Goncharov <dgoncharov@users.sf.net>
2010-07-16 13:58:59 +0400

    correct left sdp body gets proxied to the right phone

--- sippy/b2bua_radius.py
+++ sippy/b2bua_radius.py
@@ -380,24 +380,8 @@ class CallController(object):
             tmp = self.cli
             self.cli = self.cld
             self.cld = tmp
+            body = self.uaA.rSDP #TODO: a get method()?
 
-    #TODO: correct sdp body of uaA
-            t = str(time())
-            a = global_config['sip_address'] 
-            content = \
-            "v=0\r\n" \
-            "o=- 1 2 IN IP4 " + a + "\r\n" \
-            "s=b2bua\r\n" \
-            "c=IN IP4 " + a + "\r\n" \
-            "t=0 0\r\n" \
-            "a=sendrecv\r\n" \
-            "m=audio 35000 RTP/AVP 0 8 101\r\n" \
-            "a=rtpmap:0 PCMU/8000\r\n" \
-            "a=rtpmap:8 PCMA/8000\r\n" \
-            "a=rtpmap:101 telephone-event/8000\r\n" 
-
-            body = MsgBody(content)
-    #TODO end
             self.cId = SipCallId()
             self.caller_name = self.cli
             auth = None
